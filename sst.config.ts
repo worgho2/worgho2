77796e7e945782bd3ddfba1db540df692d95613a
worgho2 <otavio.baziewicz.filho@gmail.com>
2024-10-28 21:49:26 -0300

    chore: Add repository boilerplate

---
+++ sst.config.ts
@@ -0,0 +1,19 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+export default $config({
+  app(input) {
+    return {
+      name: 'worgho2',
+      removal: 'remove',
+      home: 'aws',
+      providers: { cloudflare: '5.41.0' },
+    };
+  },
+  async run() {
+    await import('./infra/env');
+    const web = await import('./infra/web');
+
+    return {
+      webNextJsAppUrl: web.nextJsApp.url,
+    };
+  },
+});
