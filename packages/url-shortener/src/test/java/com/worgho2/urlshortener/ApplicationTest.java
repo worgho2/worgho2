06a55984f5650ab43afa9262c3f6f06863fa7c4e
worgho2 <otavio.baziewicz.filho@gmail.com>
2025-03-18 17:24:17 -0300

    fix: Adjust java test

---
+++ packages/url-shortener/src/test/java/com/worgho2/urlshortener/ApplicationTest.java
@@ -0,0 +1,20 @@
+package com.worgho2.urlshortener;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+
+@MicronautTest
+class ApplicationTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+}
